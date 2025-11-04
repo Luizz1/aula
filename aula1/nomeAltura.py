65ecdaff94fc7f09f480e19b76903a6c9d5afa1f
Luiz Felipe <79612437+Luizz1@users.noreply.github.com>
2025-11-03 21:07:21 -0300

    Add files via upload

---
+++ aula1/nomeAltura.py
@@ -0,0 +1,4 @@
+nome = input("Insira o seu nome: ")
+altura = input("Insira a sua altura: ")
+idade = input("Insira a sua idade: ")
+print(f'Seu nome é {nome}, sua altura é {altura}, sua idade é {idade}')
