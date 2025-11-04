61968e4f2efba1f6b01030502b59ed404a58b5ec
Luiz Felipe <79612437+Luizz1@users.noreply.github.com>
2025-11-03 21:06:40 -0300

    Add files via upload

---
+++ aula1/cupomLoja.py
@@ -0,0 +1,3 @@
+nome = "Julio"
+valor = 400
+print(f'Olá, {nome}. Em JANEIRO você realizou uma compra no valor de R$ {valor:.2f} e ganhou um desconto de 10% em sua próxima compra. Use o cupom {nome.upper()}É10.')
