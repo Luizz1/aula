79d44c13da90b88aeab218f5a8e200251a79538b
Luiz Felipe <79612437+Luizz1@users.noreply.github.com>
2025-11-03 21:10:58 -0300

    Add files via upload

---
+++ aula2/Cálculos.py
@@ -0,0 +1,8 @@
+valor = int(input('Insira um valor: '))
+
+print(f'O dobro do valor {valor} é {valor * 2}')
+print(f'O triplo do valor {valor} é {valor * 3}')
+print(f'O valor ao quadrado de {valor} é {valor ** 2}')
+print(f'A raiz quadrada do valor {valor} é {valor ** 0.5}')
+print(f'A raíz cúbica do valor {valor} é {valor ** (1/3)}')
+
